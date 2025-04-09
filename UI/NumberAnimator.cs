1c4bc983f844428405ad3abd7d17d379db03e0e7
Tobias Graski <57156093+t-graski@users.noreply.github.com>
2025-04-09 18:38:20 +0100

    feat: add number formatter and animator

---
+++ UI/NumberAnimator.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ranch_mayhem_engine.UI;
+
+public class NumberAnimator(float startNumber, float endNumber, float animationSpeed = 0.1f)
+{
+    private readonly float _animationSpeed = animationSpeed;
+    private readonly float _startNumber = startNumber;
+    private float _currentNumber = startNumber;
+    private readonly float _endNumber = endNumber;
+
+    private bool _isAnimationDone = true;
+    private bool _isAnimating = false;
+
+    public void StartAnimation()
+    {
+        _isAnimationDone = false;
+        _isAnimating = true;
+    }
+
+    public void FinishAnimation()
+    {
+        _isAnimating = false;
+        _isAnimationDone = true;
+        _currentNumber = _endNumber;
+    }
+
+    public void Update()
+    {
+        _currentNumber = MathHelper.Lerp(_currentNumber, _endNumber, _animationSpeed);
+
+        if (Math.Abs(_currentNumber - _endNumber) < 1)
+        {
+            FinishAnimation();
+        }
+    }
+
+    public float GetCurrent() => _currentNumber;
+}
---
+++ Utils/NumberFormatter.cs
@@ -0,0 +1,60 @@
+﻿namespace ranch_mayhem_engine.Utils;
+
+public class NumberFormatter : IFormatProvider, ICustomFormatter
+{
+    public object? GetFormat(Type? formatType)
+    {
+        return formatType == typeof(ICustomFormatter) ? this : null;
+    }
+
+    public string Format(string? format, object? arg, IFormatProvider? formatProvider)
+    {
+        if (arg is not IFormattable)
+        {
+            if (format != null) return string.Format(format, arg);
+        }
+
+        var formattable = (IFormattable)arg!;
+
+        if (format?.ToUpper() == "N")
+        {
+            if (arg is double or float)
+            {
+                var number = Convert.ToDouble(arg);
+                return FormatNumber(number);
+            }
+
+            if (arg is int i)
+            {
+                return FormatNumber(i);
+            }
+
+            if (arg is long l)
+            {
+                return FormatNumber(l);
+            }
+
+            if (arg is decimal d)
+            {
+                return FormatNumber((double)d);
+            }
+        }
+
+        return formattable!.ToString(format, formatProvider);
+    }
+
+    private static string FormatNumber(double number)
+    {
+        const double trillion = 1e12;
+        const double billion = 1e9;
+        const double million = 1e6;
+
+        return number switch
+        {
+            >= trillion => $"{number / trillion:0.##}T",
+            >= billion => $"{number / billion:0.##}B",
+            >= million => $"{number / million:0.##}M",
+            _ => number.ToString($"#,0")
+        };
+    }
+}
